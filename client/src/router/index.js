15e2d4756a52047270bbec100fd03f1cca5e0c23
samaradel <samar@incubaid.com>
2023-03-20 16:56:49 +0200

    fix conflicts

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,69 +1,92 @@
 // Composables
-import { createRouter, createWebHistory } from 'vue-router'
-const routes = [{
-    path: '/',
-    component: () =>
-        import ('@/layouts/default/Default.vue'),
-    children: [{
-            path: 'login',
-            name: 'Login',
-            // route level code-splitting
-            // this generates a separate chunk (about.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () =>
-                import ( /* webpackChunkName: "login" */ '@/views/Login.vue'),
-        },
-        {
-            path: 'signup',
-            name: 'Signup',
-            // route level code-splitting
-            // this generates a separate chunk (about.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () =>
-                import ( /* webpackChunkName: "login" */ '@/views/Signup.vue'),
-        },
-        {
-            path: 'forgetPassword',
-            name: 'ForgetPassword',
-            // route level code-splitting
-            // this generates a separate chunk (about.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () =>
-                import ( /* webpackChunkName: "login" */ '@/views/Forgetpassword.vue'),
-        },
-        {
-            path: 'otp',
-            name: 'OTP',
-            // route level code-splitting
-            // this generates a separate chunk (about.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () =>
-                import ( /* webpackChunkName: "login" */ '@/views/Otp.vue'),
-        },
-        {
-            path: 'newPassword',
-            name: 'NewPassword',
-            // route level code-splitting
-            // this generates a separate chunk (about.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () =>
-                import ( /* webpackChunkName: "login" */ '@/views/Newpassword.vue'),
-        },
-        {
-            path: '',
-            name: 'Home',
-            // route level code-splitting
-            // this generates a separate chunk (about.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () =>
-                import ( /* webpackChunkName: "home" */ '@/views/Home.vue'),
-        }
+import { createRouter, createWebHistory } from "vue-router";
+import Profile from "@/views/Profile.vue";
+import Home from "@/views/Home.vue";
+import About from "@/views/About.vue";
+import VM from "@/views/VM.vue";
+import K8s from "@/views/K8s.vue";
+const routes = [
+  {
+    path: "/",
+    component: () => import("@/layouts/default/Default.vue"),
+    children: [
+      {
+        path: "login",
+        name: "Login",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+          import(/* webpackChunkName: "login" */ "@/views/Login.vue"),
+      },
+      {
+        path: "signup",
+        name: "Signup",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+          import(/* webpackChunkName: "login" */ "@/views/Signup.vue"),
+      },
+      {
+        path: "forgetPassword",
+        name: "ForgetPassword",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+          import(/* webpackChunkName: "login" */ "@/views/Forgetpassword.vue"),
+      },
+      {
+        path: "otp",
+        name: "OTP",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+          import(/* webpackChunkName: "login" */ "@/views/Otp.vue"),
+      },
+      {
+        path: "newPassword",
+        name: "NewPassword",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+          import(/* webpackChunkName: "login" */ "@/views/Newpassword.vue"),
+      },
+      {
+        path: "profile",
+        name: "Profile",
+        component: Profile,
+      },
+      {
+        path: "",
+        name: "Home",
+        component: Home,
+      },
+      {
+        path: "about",
+        name: "About",
+        component: About,
+      },
+      {
+        path: "vm",
+        name: "VM",
+        component: VM,
+      },
+      {
+        path: "k8s",
+        name: "K8s",
+        component: K8s,
+      },
     ],
-}, ]
+  },
+];
 
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
-    routes,
-})
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
 
-export default router
+export default router;

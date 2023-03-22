4bdf0774ff4680e3fc58a11d31f3f62aa394dba1
AbdelrahmanElawady <abdoelawady125@gmail.com>
2023-03-22 17:27:36 +0200

    Log internal server errors instead of sending to user

--- server/routes/quota_router.go
+++ server/routes/quota_router.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 
 	"github.com/rawdaGastan/cloud4students/middlewares"
+	"github.com/rs/zerolog/log"
+	"gorm.io/gorm"
 )
 
 // GenerateVoucherHandler generates a voucher by admin
@@ -12,8 +14,13 @@ func (r *Router) GetQuotaHandler(w http.ResponseWriter, req *http.Request) {
 	userID := req.Context().Value(middlewares.UserIDKey("UserID")).(string)
 
 	quota, err := r.db.GetUserQuota(userID)
+	if err == gorm.ErrRecordNotFound {
+		writeNotFoundResponse(w, "user quota not found")
+		return
+	}
 	if err != nil {
-		writeNotFoundResponse(w, err.Error())
+		log.Error().Err(err).Send()
+		writeErrResponse(w, internalServerErrorMsg)
 		return
 	}
 

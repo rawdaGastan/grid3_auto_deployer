8f5ae6ef2a747cbdb39a22f8c91c1abfa3505a33
alaamahmoud.1223@gmail.com <alaamahmoud.1223@gmail.com>
2023-04-10 14:22:08 +0200

    add k8s tests

--- server/routes/k8s_router_test.go
+++ server/routes/k8s_router_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/codescalers/cloud4students/internal"
 	"github.com/codescalers/cloud4students/middlewares"
 	"github.com/codescalers/cloud4students/models"
+	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -98,7 +99,7 @@ func TestK8sGetAllHandler(t *testing.T) {
 		token, err := internal.CreateJWT(user.ID.String(), user.Email, config.Token.Secret, config.Token.Timeout)
 		assert.NoError(t, err)
 
-		cluster1 := models.K8sCluster{
+		cluster := models.K8sCluster{
 			ID:     1,
 			UserID: user.ID.String(),
 			Master: models.Master{
@@ -124,18 +125,71 @@ func TestK8sGetAllHandler(t *testing.T) {
 			},
 		}
 
-		err = db.CreateK8s(&cluster1)
+		err = db.CreateK8s(&cluster)
 		assert.NoError(t, err)
 
-		cluster2 := models.K8sCluster{
-			ID:     2,
+		cluster.ID = 2
+		cluster.Master.Name = cluster.Master.Name + "3"
+
+		err = db.CreateK8s(&cluster)
+		assert.NoError(t, err)
+
+		request := httptest.NewRequest("GET", version+"/k8s", nil)
+		request.Header.Set("Authorization", fmt.Sprintf("Bearer %v", token))
+		ctx := context.WithValue(request.Context(), middlewares.UserIDKey("UserID"), user.ID.String())
+		newRequest := request.WithContext(ctx)
+		response := httptest.NewRecorder()
+		router.K8sGetAllHandler(response, newRequest)
+		assert.Equal(t, response.Code, http.StatusOK)
+
+	})
+
+	t.Run("no clusters for user", func(t *testing.T) {
+		user, err := db.GetUserByEmail("name@gmail.com")
+		assert.NoError(t, err)
+
+		token, err := internal.CreateJWT(user.ID.String(), user.Email, config.Token.Secret, config.Token.Timeout)
+		assert.NoError(t, err)
+
+		request := httptest.NewRequest("GET", version+"/k8s", nil)
+		request.Header.Set("Authorization", fmt.Sprintf("Bearer %v", token))
+		ctx := context.WithValue(request.Context(), middlewares.UserIDKey("UserID"), user.ID.String())
+		newRequest := request.WithContext(ctx)
+		response := httptest.NewRecorder()
+		router.K8sGetAllHandler(response, newRequest)
+		assert.Equal(t, response.Code, http.StatusOK)
+	})
+
+}
+
+func TestK8sDeleteAllHandler(t *testing.T) {
+	router, db, config, version := SetUp(t)
+	u := models.User{
+		Name:           "name",
+		Email:          "name@gmail.com",
+		HashedPassword: "$2a$14$EJtkQHG54.wyFnBMBJn2lus5OkIZn3l/MtuqbaaX1U3KpttvxVGN6",
+		Verified:       true,
+		SSHKey:         "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCSJYyNo6j1LxrjDTRGkbBgIyD/puMprzoepKr2zwbNobCEMfAx9DXBFstueQ9wYgcwO0Pu7/95BNgtGhjoRsNDEz5MBO0Iyhcr9hGYfoXrG2Ufr8IYu3i5DWLRmDERzuArZ6/aUWIpCfpheHX+/jH/R9vvnjO2phCutpkWrjx34/33U3pL+RRycA1uTsISZTyrcMZIXfABI4xBMFLundaBk6F4YFZaCjkUOLYld4KDxJ+N6cYnJ5pa5/hLzZQedn6h7SpMvSCghxOdCxqdEwF0m9odfsrXeKRBxRfL+HWxqytNKp9CgfLvE9Knmfn5GWhXYS6/7dY7GNUGxWSje6L1h9DFwhJLjTpEwoboNzveBmlcyDwduewFZZY+q1C/gKmJial3+0n6zkx4daQsiHc29KM5wiH8mvqpm5Ew9vWNOqw85sO7BaE1W5jMkZOuqIEJiz+KW6UicUBbv2YJ8kjvNtMLM1BiE3/WjVXQ3cMf1x1mUH4bFVgW7F42nnkuc2k= alaa@alaa-Inspiron-5537",
+	}
+	err := db.CreateUser(&u)
+	assert.NoError(t, err)
+
+	t.Run("delete all k8s of user ", func(t *testing.T) {
+		user, err := db.GetUserByEmail("name@gmail.com")
+		assert.NoError(t, err)
+
+		token, err := internal.CreateJWT(user.ID.String(), user.Email, config.Token.Secret, config.Token.Timeout)
+		assert.NoError(t, err)
+
+		cluster := models.K8sCluster{
+			ID:     1,
 			UserID: user.ID.String(),
 			Master: models.Master{
 				Name: "name",
 			},
 			Workers: []models.Worker{
 				models.Worker{
-					ClusterID: 2,
+					ClusterID: 1,
 					Name:      "worker1",
 					Resources: "small",
 					SRU:       5,
@@ -143,7 +197,7 @@ func TestK8sGetAllHandler(t *testing.T) {
 					MRU:       2,
 				},
 				models.Worker{
-					ClusterID: 2,
+					ClusterID: 1,
 					Name:      "worker2",
 					Resources: "small",
 					SRU:       5,
@@ -153,15 +207,147 @@ func TestK8sGetAllHandler(t *testing.T) {
 			},
 		}
 
-		err = db.CreateK8s(&cluster2)
+		err = db.CreateK8s(&cluster)
 		assert.NoError(t, err)
 
-		request := httptest.NewRequest("GET", version+"/k8s", nil)
+		cluster.ID = 2
+		cluster.Master.Name = cluster.Master.Name + "3"
+
+		err = db.CreateK8s(&cluster)
+		assert.NoError(t, err)
+
+		request := httptest.NewRequest("DELETE", version+"/k8s", nil)
 		request.Header.Set("Authorization", fmt.Sprintf("Bearer %v", token))
 		ctx := context.WithValue(request.Context(), middlewares.UserIDKey("UserID"), user.ID.String())
 		newRequest := request.WithContext(ctx)
 		response := httptest.NewRecorder()
-		router.K8sGetAllHandler(response, newRequest)
+		router.K8sDeleteAllHandler(response, newRequest)
+		assert.Equal(t, response.Code, http.StatusOK)
+
+	})
+}
+
+func TestK8sGetHandler(t *testing.T) {
+	router, db, config, version := SetUp(t)
+	u := models.User{
+		Name:           "name",
+		Email:          "name@gmail.com",
+		HashedPassword: "$2a$14$EJtkQHG54.wyFnBMBJn2lus5OkIZn3l/MtuqbaaX1U3KpttvxVGN6",
+		Verified:       true,
+		SSHKey:         "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCSJYyNo6j1LxrjDTRGkbBgIyD/puMprzoepKr2zwbNobCEMfAx9DXBFstueQ9wYgcwO0Pu7/95BNgtGhjoRsNDEz5MBO0Iyhcr9hGYfoXrG2Ufr8IYu3i5DWLRmDERzuArZ6/aUWIpCfpheHX+/jH/R9vvnjO2phCutpkWrjx34/33U3pL+RRycA1uTsISZTyrcMZIXfABI4xBMFLundaBk6F4YFZaCjkUOLYld4KDxJ+N6cYnJ5pa5/hLzZQedn6h7SpMvSCghxOdCxqdEwF0m9odfsrXeKRBxRfL+HWxqytNKp9CgfLvE9Knmfn5GWhXYS6/7dY7GNUGxWSje6L1h9DFwhJLjTpEwoboNzveBmlcyDwduewFZZY+q1C/gKmJial3+0n6zkx4daQsiHc29KM5wiH8mvqpm5Ew9vWNOqw85sO7BaE1W5jMkZOuqIEJiz+KW6UicUBbv2YJ8kjvNtMLM1BiE3/WjVXQ3cMf1x1mUH4bFVgW7F42nnkuc2k= alaa@alaa-Inspiron-5537",
+	}
+	err := db.CreateUser(&u)
+	assert.NoError(t, err)
+
+	t.Run("get k8s of user ", func(t *testing.T) {
+		user, err := db.GetUserByEmail("name@gmail.com")
+		assert.NoError(t, err)
+
+		token, err := internal.CreateJWT(user.ID.String(), user.Email, config.Token.Secret, config.Token.Timeout)
+		assert.NoError(t, err)
+
+		cluster := models.K8sCluster{
+			ID:     1,
+			UserID: user.ID.String(),
+			Master: models.Master{
+				Name: "name",
+			},
+			Workers: []models.Worker{
+				models.Worker{
+					ClusterID: 1,
+					Name:      "worker1",
+					Resources: "small",
+					SRU:       5,
+					CRU:       2,
+					MRU:       2,
+				},
+				models.Worker{
+					ClusterID: 1,
+					Name:      "worker2",
+					Resources: "small",
+					SRU:       5,
+					CRU:       2,
+					MRU:       2,
+				},
+			},
+		}
+
+		err = db.CreateK8s(&cluster)
+		assert.NoError(t, err)
+
+		req := httptest.NewRequest("GET", version+"/k8s/1", nil)
+		request := mux.SetURLVars(req, map[string]string{
+			"id": "1",
+		})
+
+		request.Header.Set("Authorization", fmt.Sprintf("Bearer %v", token))
+		ctx := context.WithValue(request.Context(), middlewares.UserIDKey("UserID"), user.ID.String())
+		newRequest := request.WithContext(ctx)
+		response := httptest.NewRecorder()
+		router.K8sGetHandler(response, newRequest)
+		assert.Equal(t, response.Code, http.StatusOK)
+
+	})
+}
+
+func TestK8sDeleteHandler(t *testing.T) {
+	router, db, config, version := SetUp(t)
+	u := models.User{
+		Name:           "name",
+		Email:          "name@gmail.com",
+		HashedPassword: "$2a$14$EJtkQHG54.wyFnBMBJn2lus5OkIZn3l/MtuqbaaX1U3KpttvxVGN6",
+		Verified:       true,
+		SSHKey:         "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCSJYyNo6j1LxrjDTRGkbBgIyD/puMprzoepKr2zwbNobCEMfAx9DXBFstueQ9wYgcwO0Pu7/95BNgtGhjoRsNDEz5MBO0Iyhcr9hGYfoXrG2Ufr8IYu3i5DWLRmDERzuArZ6/aUWIpCfpheHX+/jH/R9vvnjO2phCutpkWrjx34/33U3pL+RRycA1uTsISZTyrcMZIXfABI4xBMFLundaBk6F4YFZaCjkUOLYld4KDxJ+N6cYnJ5pa5/hLzZQedn6h7SpMvSCghxOdCxqdEwF0m9odfsrXeKRBxRfL+HWxqytNKp9CgfLvE9Knmfn5GWhXYS6/7dY7GNUGxWSje6L1h9DFwhJLjTpEwoboNzveBmlcyDwduewFZZY+q1C/gKmJial3+0n6zkx4daQsiHc29KM5wiH8mvqpm5Ew9vWNOqw85sO7BaE1W5jMkZOuqIEJiz+KW6UicUBbv2YJ8kjvNtMLM1BiE3/WjVXQ3cMf1x1mUH4bFVgW7F42nnkuc2k= alaa@alaa-Inspiron-5537",
+	}
+	err := db.CreateUser(&u)
+	assert.NoError(t, err)
+
+	t.Run("delete k8s of user ", func(t *testing.T) {
+		user, err := db.GetUserByEmail("name@gmail.com")
+		assert.NoError(t, err)
+
+		token, err := internal.CreateJWT(user.ID.String(), user.Email, config.Token.Secret, config.Token.Timeout)
+		assert.NoError(t, err)
+
+		cluster := models.K8sCluster{
+			ID:     1,
+			UserID: user.ID.String(),
+			Master: models.Master{
+				Name: "name",
+			},
+			Workers: []models.Worker{
+				models.Worker{
+					ClusterID: 1,
+					Name:      "worker1",
+					Resources: "small",
+					SRU:       5,
+					CRU:       2,
+					MRU:       2,
+				},
+				models.Worker{
+					ClusterID: 1,
+					Name:      "worker2",
+					Resources: "small",
+					SRU:       5,
+					CRU:       2,
+					MRU:       2,
+				},
+			},
+		}
+
+		err = db.CreateK8s(&cluster)
+		assert.NoError(t, err)
+
+		req := httptest.NewRequest("DELETE", version+"/k8s/1", nil)
+		request := mux.SetURLVars(req, map[string]string{
+			"id": "1",
+		})
+
+		request.Header.Set("Authorization", fmt.Sprintf("Bearer %v", token))
+		ctx := context.WithValue(request.Context(), middlewares.UserIDKey("UserID"), user.ID.String())
+		newRequest := request.WithContext(ctx)
+		response := httptest.NewRecorder()
+		router.K8sDeleteHandler(response, newRequest)
 		assert.Equal(t, response.Code, http.StatusOK)
 
 	})

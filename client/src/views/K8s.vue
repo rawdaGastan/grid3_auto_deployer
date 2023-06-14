25278ddae7713081f59f392184a030342df91393
rawdaGastan <rody.rawda.rr@gmail.com>
2023-06-14 16:42:42 +0300

    fix name validations

--- client/src/views/K8s.vue
+++ client/src/views/K8s.vue
@@ -164,7 +164,7 @@ export default {
 		const nameValidation = ref([
 			(value) => {
 				if (value.length < 3 || value.length > 20) return "Name needs to be more than 2 characters and less than 20";
-				if (!/^[a-z]+$/.test(value)) return "Name allows lowercase characters only";
+				if (!/^[a-z]+$/.test(value)) return "Name can only include lowercase alphanumeric characters";
 				return true;
 			},
 			(value) => validateK8sName(value),

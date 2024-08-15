7f1bc831894e734dfc6fe8451304998f31cb0ff4
rowanxgamal <rowangamalelkhouly@gmail.com>
2024-08-15 16:54:46 +0300

    feat: refined error messages of user authorization

--- server/middlewares/authorization.go
+++ server/middlewares/authorization.go
@@ -21,26 +21,26 @@ func Authorization(db models.DB, secret string, timeout int) func(http.Handler) 
 			reqToken := r.Header.Get("Authorization")
 			splitToken := strings.Split(reqToken, "Bearer ")
 			if len(splitToken) != 2 {
-				writeErrResponse(r, w, http.StatusUnauthorized, "user is not authorized")
+				writeErrResponse(r, w, http.StatusUnauthorized, "user is not authorized, problem with token parsing")
 				return
 			}
 
 			if strings.TrimSpace(splitToken[1]) == "" {
-				writeErrResponse(r, w, http.StatusUnauthorized, "user is not authorized")
+				writeErrResponse(r, w, http.StatusUnauthorized, "user is not authorized, problem with token parsing")
 				return
 			}
 			reqToken = splitToken[1]
 
 			claims, err := internal.ValidateJWTToken(reqToken, secret, timeout)
 			if err != nil {
-				writeErrResponse(r, w, http.StatusUnauthorized, "user is not authorized")
+				writeErrResponse(r, w, http.StatusUnauthorized, "user is not authorized, token is not valid")
 				return
 			}
 			ctx := context.WithValue(r.Context(), UserIDKey("UserID"), claims.UserID)
 
 			user, err := db.GetUserByID(claims.UserID)
 			if err == gorm.ErrRecordNotFound {
-				writeErrResponse(r, w, http.StatusNotFound, "user is not found")
+				writeErrResponse(r, w, http.StatusNotFound, "user: "+ user.Email +", is not found")
 				return
 			}
 			if err != nil {
@@ -48,7 +48,7 @@ func Authorization(db models.DB, secret string, timeout int) func(http.Handler) 
 				return
 			}
 			if !user.Verified {
-				writeErrResponse(r, w, http.StatusBadRequest, "email is not verified yet, please check the verification email in your inbox")
+				writeErrResponse(r, w, http.StatusBadRequest, "email: "+ user.Email +" is not verified yet, please check the verification email in your inbox")
 				return
 			}
 

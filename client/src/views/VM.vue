96c73f8df9b281bd65e03b992d1b164d05fbb43c
samaradel <samar@incubaid.com>
2024-12-10 15:30:20 +0200

    Fix vm page

--- client/src/views/VM.vue
+++ client/src/views/VM.vue
@@ -4,113 +4,105 @@
       You will not be able to deploy. Please add your public SSH key in your
       profile settings.
     </v-alert>
-    <h5 class="text-h5 text-md-h4 font-weight-bold text-center mt-10 secondary">
-      Virtual Machines
-    </h5>
-    <p class="text-center mb-10">
-      Deploy a new virtual machine
-    </p>
-    <v-row justify="center">
-      <v-col cols="12" sm="6" xl="4">
-        <v-form v-model="verify" ref="form" @submit.prevent="deployVm">
-          <v-text-field
-            label="Name"
-            :rules="nameValidation"
-            class="my-2"
-            v-model="name"
-            bg-color="accent"
-            variant="outlined"
-            density="compact"
-          ></v-text-field>
-          <BaseSelect
-            :modelValue="selectedResource"
-            :items="resources"
-            :reduce="(sel) => sel.value"
-            placeholder="Resources"
-						:rules="[() => !!selectedResource || 'This field is required']"
-            @update:modelValue="selectedResource = $event"
-          />
-          <v-checkbox v-model="checked" label="Public IP"></v-checkbox>
+    <h5 class="text-h5 text-md-h4 font-weight-bold my-5">Virtual Machines</h5>
+    <v-divider />
+    <v-row class="d-flex justify-end my-5">
+      <v-dialog max-width="500">
+        <template v-slot:activator="{ props: activatorProps }">
           <BaseButton
-            type="submit"
-            block
-            class="bg-primary"
-            :loading="loading"
-            :disabled="!verify || alert"
-            text="Deploy"
+            v-bind="activatorProps"
+            color="error"
+            :loading="deLoading"
+            class="mr-3"
+            :disabled="results.length == 0"
+            text="Delete All"
           />
-        </v-form>
-      </v-col>
+        </template>
+
+        <template v-slot:default="{ isActive }">
+          <v-card class="pa-2">
+            <v-card-title>Delete</v-card-title>
+            <v-divider />
+            <v-card-text> Are you sure you need to delete card? </v-card-text>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+
+              <BaseButton
+                text="Cancel"
+                @click="isActive.value = false"
+                variant="outlined"
+              />
+
+              <BaseButton
+                type="submit"
+                text="Yes Delete"
+                color="error"
+                rounded="lg"
+                @click="deleteVms"
+              />
+            </v-card-actions>
+          </v-card>
+        </template>
+      </v-dialog>
+
+      <BaseButton
+        color="secondary"
+        :loading="deLoading"
+        @click="createVM"
+        text="+ Create a new VM"
+      />
     </v-row>
-    <v-row v-if="results.length > 0">
-      <v-col class="d-flex justify-end">
-        <BaseButton
-          color="red-accent-2"
-          :loading="deLoading"
-          @click="deleteVms"
-          text="Delete All"
-        />
-      </v-col>
-    </v-row>
-    <v-row v-if="results.length > 0">
-      <v-col>
-        <v-row>
-          <v-col>
-            <v-data-table
-              :headers="headers"
-              :items="results"
-              class="elevation-1"
+    <v-row
+      ><v-col cols="12">
+        <v-data-table
+          :headers="headers"
+          :items="results"
+          class="d-flex justify-center elevation-1"
+        >
+          <template #[`item.id`]="{ item }">
+            {{ results.indexOf(item) + 1 }}
+          </template>
+          <template #[`item.ygg_ip`]="{ item }">
+            {{ item.ygg_ip || "-" }}
+            <v-icon
+              size="small"
+              v-if="!item.deleting"
+              class="secondary cursor-pointer mx-2"
+              @click="copyIP(item.ygg_ip)"
             >
-              <template v-slot:item="{ item }">
-                <tr>
-                  <td>{{ item.id }}</td>
-                  <td>{{ item.name }}</td>
-                  <td>{{ item.sru }}GB</td>
-                  <td>{{ item.mru }}GB</td>
-                  <td>{{ item.cru }}</td>
-                  <td class="cursor-pointer" @click="copyIP(item.ygg_ip)">
-                    {{ item.ygg_ip }}
-                  </td>
-                  <td class="cursor-pointer" @click="copyIP(item.mycelium_ip)">
-                    {{ item.mycelium_ip }}
-                  </td>
-                  <td
-                    v-if="item.public_ip"
-                    class="cursor-pointer"
-                    @click="copyIP(item.public_ip)"
-                  >
-                    {{ item.public_ip }}
-                  </td>
-                  <td v-else>-</td>
-                  <td>
-                    <font-awesome-icon
-                      v-if="!item.deleting"
-                      class="text-red-accent-2 cursor-pointer"
-                      @click="deleteVm(item)"
-                      icon="fa-solid fa-trash"
-                    />
-                    <v-progress-circular
-                      v-else
-                      indeterminate
-                      color="red"
-                      size="20"
-                    ></v-progress-circular>
-                  </td>
-                </tr>
-              </template>
-            </v-data-table>
-          </v-col>
-        </v-row>
+              mdi-content-copy
+            </v-icon>
+          </template>
+
+          <template #[`item.public_ip`]="{ item }">
+            {{ item.public_ip || "-" }}
+          </template>
+
+          <template #[`item.actions`]="{ item }">
+            <v-icon
+              size="small"
+              v-if="!item.deleting"
+              class="secondary cursor-pointer"
+              @click="deleteVm(item)"
+            >
+              mdi-delete
+            </v-icon>
+            <v-progress-circular
+              v-else
+              indeterminate
+              color="red"
+              size="20"
+            ></v-progress-circular>
+          </template>
+          <template #no-data>
+            <p class="text-capitalize text-h6 pa-16 text-disabled">
+              Create new virtual machine
+            </p>
+          </template>
+        </v-data-table>
       </v-col>
     </v-row>
-    <v-row v-else>
-      <v-col>
-        <p class="my-5 text-center">
-          You don't have any Virtual machines deployed yet
-        </p>
-      </v-col>
-    </v-row>
-    <Confirm ref="confirm" />
     <Toast ref="toast" />
   </v-container>
 </template>
@@ -118,32 +110,22 @@
 <script>
 import { ref, onMounted, inject } from "vue";
 import userService from "@/services/userService";
-import BaseSelect from "@/components/Form/BaseSelect.vue";
 import BaseButton from "@/components/Form/BaseButton.vue";
-import Confirm from "@/components/Confirm.vue";
 import Toast from "@/components/Toast.vue";
-
+import { useRouter } from "vue-router";
 export default {
   components: {
-    BaseSelect,
     BaseButton,
-    Confirm,
     Toast,
   },
   setup() {
     const emitter = inject("emitter");
-    const verify = ref(false);
-    const checked = ref(false);
     const alert = ref(false);
     const itemsPerPage = ref(null);
-    const name = ref("");
     const confirm = ref(null);
     const selectedResource = ref("");
-    const resources = ref([
-      { title: "Small VM (1 CPU, 2GB, 25GB)", value: "small" },
-      { title: "Medium VM (2 CPU, 4GB, 50GB)", value: "medium" },
-      { title: "Large VM (4 CPU, 8GB, 100GB)", value: "large" },
-    ]);
+    const dialog = ref(null);
+    const router = useRouter();
     const headers = ref([
       {
         title: "ID",
@@ -176,82 +158,35 @@ export default {
         sortable: false,
       },
       {
-        title: "Mycelium IP",
-        key: "mycelium_ip",
-        sortable: false,
-      },
-      {
         title: "Public IP",
         key: "public_ip",
         sortable: false,
       },
+
       { title: "Actions", key: "actions", sortable: false },
     ]);
 
     const toast = ref(null);
-    const loading = ref(false);
     const results = ref([]);
     const deLoading = ref(false);
     const message = ref(null);
-    const form = ref(null);
-    const nameValidation = ref([
-      (value) => {
-        if (value && (value.length < 3 || value.length > 20))
-          return "Name needs to be more than 2 characters and less than 20";
-        if (!/^[a-z]+$/.test(value))
-          return "Name can only include lowercase alphabetic characters";
-        return true;
-      },
-      (value) => validateVMName(value),
-    ]);
-    
+
     const getVMS = () => {
       userService
         .getVms()
         .then((response) => {
-          const { data } = response.data;
+          const { data, msg } = response.data;
           data.map((item) => {
             item.deleting = false;
             item.public_ip = item.public_ip.split("/")[0];
           });
           results.value = data;
+          message.value = msg;
         })
         .catch((response) => {
           const { err } = response.response.data;
           toast.value.toast(err, "#FF5252");
         });
-    };
-
-    const deployVm = () => {
-      loading.value = true;
-      userService
-        .deployVm(name.value, selectedResource.value, checked.value)
-        .then((response) => {
-          toast.value.toast(response.data.msg, "#388E3C");
-          emitQuota();
-          getVMS();
-        })
-        .catch((response) => {
-          const { err } = response.response.data;
-          toast.value.toast(err, "#FF5252");
-        })
-        .finally(() => {
-          reset();
-          loading.value = false;
-        });
-    };
-
-    const validateVMName = async (name) => {
-      var msg = "";
-      await userService.validateVMName(name).catch((response) => {
-        const { err } = response.response.data;
-        msg = err;
-      });
-
-      if (!msg) {
-        return true;
-      }
-      return msg;
     };
 
     const deleteVms = () => {
@@ -276,9 +211,6 @@ export default {
               });
           }
         });
-    };
-    const reset = () => {
-      form.value.reset();
     };
 
     const deleteVm = (item) => {
@@ -329,36 +261,34 @@ export default {
       }, 30 * 1000);
     }
 
+    function createVM() {
+      router.push({
+        name: "Deploy",
+      });
+    }
+
     onMounted(() => {
       let token = localStorage.getItem("token");
       if (token) getVMS();
     });
 
     return {
-      verify,
-      name,
       alert,
       selectedResource,
-      resources,
-      loading,
       deLoading,
       results,
       headers,
       confirm,
       toast,
       message,
-      form,
-      checked,
-      nameValidation,
       itemsPerPage,
-      reset,
       getVMS,
-      validateVMName,
-      deployVm,
       deleteVms,
       deleteVm,
       emitQuota,
       copyIP,
+      dialog,
+      createVM,
     };
   },
 };
@@ -370,7 +300,9 @@ export default {
 }
 
 thead th {
-  background-color: #217dbb !important;
-  color: white !important;
+  background-color: #19647e !important;
+}
+tbody tr {
+  background-color: #474747;
 }
 </style>
